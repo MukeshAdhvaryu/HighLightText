61bfe8667e6dcfa5ecc0b140b528847f48a19729
Mukesh Adhvaryu <maadhvaryu@gmail.com>
2016-05-04 14:14:19 +0530

    Highlight Portion of text
    It accurately highlights portion of text.

---
+++ HighlightTextDemo.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace HowToHighlightPortionOfText
+{
+    partial class HilightTextDemo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.HideSelection = false;
+            this.textBox1.Location = new System.Drawing.Point(87, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(860, 23);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "StackOverflow is a wonderful site";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkOrange;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(87, 88);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(166, 25);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Selection BackColor Start";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Gold;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(260, 88);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(166, 25);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Selection BackColor End";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(432, 88);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(166, 25);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Selection Text Forecolor";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Enter Text";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(84, 63);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(665, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "After you Enter Text, hilight portion of text in textbox and click Go button to s" +
+    "ee the result";
+            // 
+            // button4
+            // 
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Location = new System.Drawing.Point(880, 76);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(67, 32);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Go";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // HilightTextDemo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1024, 512);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "HilightTextDemo";
+            this.Text = "Highlight Text Demo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ HighlightTextDemo.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HowToHighlightPortionOfText
+{
+    public partial class HilightTextDemo : Form
+    {
+        public HilightTextDemo()
+        {
+            InitializeComponent();
+            textBox1.Text = "StackOverflow is a wonderful site";
+            textBox1.SelectionStart = 19;
+            textBox1.SelectionLength = 9;
+            enableDoubleBuffering();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+            button3.BackColor = colorDialog1.Color;
+            button1.BackColor = colorDialog1.Color;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+            button2.BackColor = colorDialog1.Color;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+            button3.ForeColor = colorDialog1.Color;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (textBox1.TextLength == 0 || textBox1.SelectionLength == 0)
+            {
+                MessageBox.Show("You must enter text in textbox and also select portion in it");
+                return;
+            }
+            using (var g = this.CreateGraphics())
+            {
+                int start = textBox1.SelectionStart;
+                int end = start + textBox1.SelectionLength - 1;
+                var rc = new Rectangle(button1.Left, 137, 860, 90);
+
+                Invalidate(rc);
+                Update();
+                g.HighlightText(textBox1.Font, textBox1.Text, rc,
+                    start, end, button3.ForeColor, button1.BackColor, button2.BackColor);
+            }
+        }
+        void enableDoubleBuffering()
+        {
+            this.SetStyle(ControlStyles.OptimizedDoubleBuffer |
+                ControlStyles.UserPaint |
+                ControlStyles.AllPaintingInWmPaint |
+                 ControlStyles.ResizeRedraw |
+                ControlStyles.SupportsTransparentBackColor, true);
+            this.UpdateStyles();
+        }
+    }
+}
